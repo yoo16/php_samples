8b6c8218c2e5f602d8dfc23ecb6b0acae3f20f5a
yoo16 <yohei.yoshikawa@gmail.com>
2021-06-15 10:26:41 +0900

    UC

--- mvc/fin/input.php
+++ mvc/fin/input.php
@@ -2,44 +2,7 @@
 require_once 'models/Member.php';
 require_once 'helpers/Form.php';
 require_once 'helpers/Session.php';
+require_once 'controllers/RegistController.php';
 
-$member = Session::load('member');
-if (!$member) {
-    $member = new Member();
-}
-// var_dump($member->value);
-include('views/regist/input.php');
-
-// session_start();
-// session_regenerate_id(true);
-//
-//会員情報初期化
-// $member = [
-//     'name' => '',
-//     'kana' => '',
-//     'email' => '',
-//     'password' => '',
-//     'tel' => '',
-//     'year' => 1980,
-//     'gender' => 'male',
-//     'q1' => '',
-// ];
-//
-//セッション取得
-// if (isset($_SESSION['member'])) {
-//     $member = $_SESSION['member'];
-// }
-// 
-// function checked($value, $target)
-// {
-//     if (is_array($value)) {
-//         return (in_array($target, $value)) ? 'checked' : '';
-//     } else {
-//         return ($value == $target) ? 'checked' : '';
-//     }
-// }
-
-// function selected($value, $target)
-// {
-//     return ($value == $target) ? 'selected' : '';
-// }
+$controller = new RegistController();
+$controller->input();
